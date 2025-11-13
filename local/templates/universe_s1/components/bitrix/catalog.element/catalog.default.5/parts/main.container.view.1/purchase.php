cb02c8cfda4aec571d08a4ea2b8154601c50462e
TomilovVA2003 <104489471+TomilovVA2003@users.noreply.github.com>
2025-11-13 14:20:05 +0300

    detail card 3

--- local/templates/universe_s1/components/bitrix/catalog.element/catalog.default.5/parts/main.container.view.1/purchase.php
+++ local/templates/universe_s1/components/bitrix/catalog.element/catalog.default.5/parts/main.container.view.1/purchase.php
@@ -44,6 +44,9 @@ use intec\core\helpers\Html;
                 <?php if ($arVisual['PRICE']['SHOW'])
                     include(__DIR__.'/../purchase/table.offer.php');
                 ?>
+                <?php if ($arVisual['PRICE']['SHOW'])
+                    include(__DIR__.'/../purchase/add.an.application.php');
+                ?>
                 <?php if ($arVisual['MEASURES']['USE'] && $arVisual['MEASURES']['POSITION'] === 'top')
                     include(__DIR__.'/../purchase/measures.php');
                 ?>
@@ -94,25 +97,27 @@ use intec\core\helpers\Html;
                     <?php } ?>
                     <div class="catalog-element-purchase-block catalog-element-purchase-action">
                         <div class="intec-grid intec-grid-wrap">
-                            <?php if ($arVisual['COUNTER']['SHOW']) { ?>
-                                <div class="intec-grid-item-2">
-                                    <?php include(__DIR__.'/../purchase/counter.php') ?>
+                            <div class="block-buy-summary-custom">
+                                <?php if ($arVisual['COUNTER']['SHOW']) { ?>
+                                    <div class="intec-grid-item-2">
+                                        <?php include(__DIR__.'/../purchase/counter.php') ?>
+                                    </div>
+                                <?php } ?>
+                                <div class="intec-grid-item">
+                                    <?php include(__DIR__.'/../purchase/order.php') ?>
                                 </div>
-                            <?php } ?>
-                            <div class="intec-grid-item">
-                                <?php include(__DIR__.'/../purchase/order.php') ?>
+                                <?php if ($arResult['ORDER_FAST']['USE']) { ?>
+                                    <div class="catalog-element-buy-fast-container intec-grid-item-1">
+                                        <?= Html::tag('div', Loc::getMessage('C_CATALOG_ELEMENT_DEFAULT_5_TEMPLATE_BUY_BUTTON_ORDER_FAST'), [
+                                            'class' => [
+                                                'catalog-element-buy-fast',
+                                                'intec-cl-text'
+                                            ],
+                                            'data-role' => 'orderFast'
+                                        ]) ?>
+                                    </div>
+                                <?php } ?>
                             </div>
-                            <?php if ($arResult['ORDER_FAST']['USE']) { ?>
-                                <div class="catalog-element-buy-fast-container intec-grid-item-1">
-                                    <?= Html::tag('div', Loc::getMessage('C_CATALOG_ELEMENT_DEFAULT_5_TEMPLATE_BUY_BUTTON_ORDER_FAST'), [
-                                        'class' => [
-                                            'catalog-element-buy-fast',
-                                            'intec-cl-text'
-                                        ],
-                                        'data-role' => 'orderFast'
-                                    ]) ?>
-                                </div>
-                            <?php } ?>
                             <?php if ($bRecalculation) { ?>
                                 <div class="catalog-element-purchase-summary intec-grid-item-1" data-role="item.summary" style="display: none">
                                     <div class="catalog-element-purchase-summary-wrapper">
@@ -121,6 +126,18 @@ use intec\core\helpers\Html;
                                     </div>
                                 </div>
                             <?php } ?>
+                        </div>
+                        <div class="container-btn-order-sample">
+                            <div class="btn-order-sample">
+                                Заказать образец
+                                <span>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                                        <path d="M12 20.5C16.9705 20.5 21 16.339 21 13.5C21 10.661 16.9705 6.5 12 6.5C7.0295 6.5 3 10.664 3 13.5C3 16.336 7.0295 20.5 12 20.5Z" stroke="#222222" stroke-width="1.5" stroke-linejoin="round"/>
+                                        <path d="M12 16.5C12.7956 16.5 13.5587 16.1839 14.1213 15.6213C14.6839 15.0587 15 14.2956 15 13.5C15 12.7044 14.6839 11.9413 14.1213 11.3787C13.5587 10.8161 12.7956 10.5 12 10.5C11.2044 10.5 10.4413 10.8161 9.87868 11.3787C9.31607 11.9413 9 12.7044 9 13.5C9 14.2956 9.31607 15.0587 9.87868 15.6213C10.4413 16.1839 11.2044 16.5 12 16.5Z" stroke="#222222" stroke-width="1.5" stroke-linejoin="round"/>
+                                        <path d="M6.63281 5.633L7.92981 7.443M17.8133 5.855L16.5158 7.665M12.0053 3.5V6.5" stroke="#222222" stroke-width="1.5" stroke-linecap="round"/>
+                                    </svg>
+                                </span>
+                            </div>
                         </div>
                     </div>
                 <?php } ?>
@@ -151,3 +168,9 @@ use intec\core\helpers\Html;
         </div>
     <?php } ?>
 </div>
+<style>
+    .intec-cl-background {
+        background-color: #744A9E !important;
+        fill: #744A9E !important;
+    }
+</style>
---
+++ local/templates/universe_s1/components/bitrix/catalog.element/catalog.default.5/parts/purchase/add.an.application.php
@@ -0,0 +1,28 @@
+<?php if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) die();
+
+use intec\core\helpers\ArrayHelper;
+use intec\core\helpers\FileHelper;
+use intec\core\helpers\Html;
+
+/**
+ * @var array $arResult
+ * @var array $arVisual
+ * @var array $arSvg
+ */
+?>
+<?/*
+global $USER;
+if ($USER->IsAuthorized() && $USER->IsAdmin()): 
+*/?>
+
+<div class="container-btn-add-an-applocation">
+    <div class="btn-add-an-applocation">
+        Добавить нанесение 
+        <span>
+            <svg xmlns="http://www.w3.org/2000/svg" width="12" height="7" viewBox="0 0 12 7" fill="none">
+                <path d="M1 0.999822L6 5.18164L11 0.999823" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+            </svg>
+        </span>
+    </div>
+</div>
+<?/* endif; */?>
