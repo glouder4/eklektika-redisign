7ecd1fb537cd6739219c9f0df01e6074c05db10d
glouder4 <egorov.2001@bk.ru>
2026-01-29 18:22:38 +0500

    Вес из характеристики предложения!

--- local/templates/universe_s1/components/bitrix/catalog.element/catalog.default.5/modifiers/catalog.php
+++ local/templates/universe_s1/components/bitrix/catalog.element/catalog.default.5/modifiers/catalog.php
@@ -1,5 +1,6 @@
 <?php if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) die();
 
+
 if (!empty($arResult['OFFERS'])) {
     $arResult['CAN_BUY'] = false;
 
@@ -15,7 +16,18 @@ if (!empty($arResult['OFFERS'])) {
             if ($arPrice === null || $arPrice['DISCOUNT_VALUE'] > $arOffer['MIN_PRICE']['DISCOUNT_VALUE'])
                 $arPrice = $arOffer['MIN_PRICE'];
 
+
+        if( isset($_GET['offer']) && !empty($_GET['offer']) ){
+            if( $arOffer['ID'] == $_GET['offer'] ){
+                //$selectedOffer = $arOffer;
+                $arResult['PRODUCT']['WEIGHT'] = $arOffer['PRODUCT']['WEIGHT'];
+            }
+        }
+
         unset($arOffer);
+    }
+    if( !isset($_GET['offer']) ){
+        $arResult['PRODUCT']['WEIGHT'] = $arResult['OFFERS'][0]['PRODUCT']['WEIGHT'];
     }
 
     $arResult['MIN_PRICE'] = $arPrice;
